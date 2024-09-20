92d112eeed2f5f7b8686ce8fe616573cc373e35e
YeetingLin <a110223044@mail.shu.edu.tw>
2024-09-20 11:46:18 +0800

    113-9-20

---
+++ 113-9-20/Range.java
@@ -0,0 +1,13 @@
+public class Range {
+    public class Range {
+        public static void main(String[] args) {
+            // byte、short、int、long 範圍
+            System.out.printf("%d ~ %d%n", 
+                    Byte.MIN_VALUE, Byte.MAX_VALUE);
+            System.out.printf("%d ~ %d%n", 
+                    Short.MIN_VALUE, Short.MAX_VALUE);
+            System.out.printf("%d ~ %d%n", 
+                    Integer.MIN_VALUE, Integer.MAX_VALUE);
+            System.o
+    
+}
