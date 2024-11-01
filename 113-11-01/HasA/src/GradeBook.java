e039a57a9c8e6ca67941b40ab8dbd7e226612b5a
YeetingLin <a110223044@mail.shu.edu.tw>
2024-11-01 10:57:36 +0800

    113-11-01

--- 113-11-01/HasA/src/GradeBook.java
+++ 113-11-01/HasA/src/GradeBook.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class GradeBook {
 private  String courseName;
 private  Student[] students;
@@ -15,6 +17,35 @@ public  GradeBook(String courseName , Student[] students , int numberOfStudents)
 public  void displayMessage(){
     System.out.printf("歡迎使用%s 成績系統\n", courseName);
 }
+
+    public  void processsStudentsGrades(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("輸入學生成績:");
+        for(int i = 0; i< numberOfStudents;i++){
+            students[i].printData();
+            System.out.print(" 的成績:");
+            grades[i] = input.nextDouble();
+
+        }
+    }
+    public  double getMaximum(){
+    double max = grades[0];
+    for(int i =i; i< numberOfStudents;i++){
+        if(grades[i]< min){
+            min = grades[i];
+        }
+    }
+    return  min;
+    }
+    public  double getAverage() {
+    double sum = 0;
+    for(int i=0;i<numberOfStudents;i++){
+        sum += grades[i];
+    }
+    return  sum/ numberOfStudents;
+    }
+
+
 public  void processsStudents(){
     for(Student student :students){
         student.printData();
